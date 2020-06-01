5ab5a4c0ae90eaae2386f6dfe4cab0d1a2f9844d
fishstack1142 <fishstack1142@gmail.com>
2020-06-01 15:09:34 +0700

    add mongodb to server /a path

--- server.js
+++ server.js
@@ -1,15 +1,67 @@
 const express = require('express')
 const next = require('next')
+const { MongoClient } = require("mongodb");
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+
 app.prepare().then(() => {
   const server = express()
 
+
+// const url = "mongodb+srv://admin1:2admin@fishstack1142-g7g61.gcp.mongodb.net/test2?retryWrites=true&w=majority";
+const url = `mongodb+srv://${process.env.mongodb_user}:${process.env.mongodb_pass}@fishstack1142-g7g61.gcp.mongodb.net/${process.env.mongodb_table}?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(url);
+ 
+ // The database to use
+ const dbName = `${process.env.mongodb_table}`;
+
   server.get('/a', (req, res) => {
+
+                      
+ async function run() {
+    try {
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db(dbName);
+
+         // Use the collection "people"
+         const col = db.collection("people");
+
+         // Construct a document                                                                                                                                                              
+         let personDocument = {
+             "name": { "first": "Ellen", "last": "Touring" },
+             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+             "views": 1250000
+         }
+
+         // Insert a single document, wait for promise so we can read it back
+         const p = await col.insertOne(personDocument);
+         // Find one document
+         const myDoc = await col.findOne();
+         // Print to the console
+         console.log(myDoc);
+
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+
     return app.render(req, res, '/a', req.query)
   })
 
