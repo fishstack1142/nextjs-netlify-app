6157da2bcab7038fff1ed2f73c7166c7293700c9
fishstack1142 <fishstack1142@gmail.com>
2020-06-01 16:42:15 +0700

    fix div tag error

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,20 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-
-    <h6>new version 15.14</h6>
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
+    <div>
+        <h6>version 15.14</h6>
+        <ul>
+          <li>
+            <Link href="/b" as="/a">
+              <a>a</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/a" as="/b">
+              <a>b</a>
+            </Link>
+          </li>
+        </ul>
+    </div>
   )
 }
